b27ed7154fdefa1921a13cb8ab71a9731d86ab18
Alexander Walker <alexanderwalker00@hotmail.com>
2023-03-03 19:51:46 +0000

    fixed high hold stack issues, buffed chords/holds

--- pulsus patterns.py
+++ pulsus patterns.py
@@ -7,7 +7,7 @@ rate = float(input("rate? "))
 rate = min(max(rate, 0.5), 2)
 for i in range(len(notes)): # get note data - position, time, hold, hold time (if a hold)
     temp = notes[i][1:-1].split(",")
-    notes[i] = [int(temp[0]),int(float(temp[1])*(1000/rate)),bool(temp[2]),int(float(temp[3])*(1000/rate))]
+    notes[i] = [int(temp[0]),int(float(temp[1])*(1000/rate)),(temp[2] == "1"),int(float(temp[3])*(1000/rate))]
 
 strain_notes = []
 hand = True
@@ -31,30 +31,28 @@ for i in range(len(notes)): # handing / strain pass - add pattern buffs later
     
     if i != 0:
         if notes[i-1][1] + 20 > note[1]: # chord detection, with a small chording window
-            current_strain[hand] = max(current_strain[hand] / 2, 0.5)
+            current_strain[hand] = (0.5+current_strain[hand] / 1.5)
         else: # do strain right
             strain_exp[hand] = (note[1] - notes[last_notes[hand]][1]) / 1000
             current_strain[hand] *= (0.33 ** strain_exp[hand])
-            current_strain[hand] += (5 + ((len(hold_stack) / 1.5))) / 10
+            current_strain[hand] += (5 + (len(hold_stack) * 2)) / 10
             last_notes[hand] = i
     note_strain = current_strain[hand]
     
-    if note[2]: # hold
-        hold_stack.append(note[1] + note[3])
-        map_length = max((note[1] + note[3]), map_length)
-        max_holds = max(max_holds, len(hold_stack))
+    if note[2] == True: # hold
+        hold_stack.append(int(note[1]) + int(note[3]))
+        map_length = max(hold_stack[-1], map_length)
     else:
         map_length = max(note[1], map_length)
 
-    strain_notes.append([note_strain * (not hand), note_strain * hand])
+    strain_notes.append(note_strain)
     notes[i].append(hand)
 
-note_times = [note[1] for note in notes] # only time values
+note_times = [note[1] for note in notes]
 note_hands = [note[4] for note in notes]
 start = min(note_times)
 section_strains = []
 section = []
-current_strain = [0,0]
 
 while start <= map_length: # section pass
     section = [x for x in note_times if (start <= x < start + 400)]
@@ -62,7 +60,7 @@ while start <= map_length: # section pass
     for note in section:
         note_id = note_times.index(note)
         hand = notes[note_id][4]
-        current_strain[hand] += strain_notes[note_id][hand]
+        current_strain[hand] += strain_notes[note_id]
     section_strains.append((current_strain[0])+(current_strain[1]))
     start += 400
 
@@ -71,6 +69,5 @@ section_strains = [x for x in section_strains if x > 0]
 for i in range(len(section_strains)):
     section_strains[i] *= 0.92**i
 
-print((sum(section_strains)/20)**0.7)
-print(max_holds)
+print((sum(section_strains)/14)**0.625)
 input()
