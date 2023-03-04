c80fbf7e7d566a30b24ace3968941d315e72f153
Alexander Walker <alexanderwalker00@hotmail.com>
2023-03-04 10:51:49 +0000

    buffed chords, and dampened high strain sections

--- pulsus patterns.py
+++ pulsus patterns.py
@@ -19,7 +19,7 @@ note_strain = 0
 map_length = 0
 max_holds = 0
 streak = 0
-streak_multipliers = [0,1,0.8,1.05,1.2,1]
+streak_multipliers = [0,0.9,0.8,1.05,1.2,0.9]
 for i in range(len(notes)): # handing / strain pass - add pattern buffs later
     note = notes[i] # get current note
 
@@ -40,11 +40,11 @@ for i in range(len(notes)): # handing / strain pass - add pattern buffs later
 
     if i != 0:
         if notes[i-1][1] + 20 > note[1]: # chord detection, with a small chording window
-            current_strain[hand] = (0.15 + current_strain[hand] / 1.5)
+            current_strain[hand] = (0.15 + current_strain[hand] / 1.2)
         else: # do strain right
             vert_notes = [note[0]//3,notes[last_notes[hand]][0]//3]
             strain_exp[hand] = (note[1] - notes[last_notes[hand]][1]) / 1000
-            current_strain[hand] += (4 + (len(hold_stack) * 2)) * (streak_multipliers[min(streak,4)]) / (15 - abs(vert_notes[0] - vert_notes[1]))
+            current_strain[hand] += (4 + (len(hold_stack) * 2)) * (streak_multipliers[min(streak,4)]) / ((15+strain_notes[-1]) - abs(vert_notes[0] - vert_notes[1]))
             current_strain[hand] *= (0.66 ** strain_exp[hand])
             last_notes[hand] = i
     note_strain = current_strain[hand]
@@ -80,8 +80,10 @@ while start <= map_length: # section pass
 section_strains.sort(reverse=True)
 section_strains = [x for x in section_strains if x > 0]
 for i in range(len(section_strains)):
-    section_strains[i] /= (4+i)
+    section_strains[i] *= (0.92**i) # old method - may end up replacing with some form of commented line
+    #section_strains[i] /= (4+i)
 
-print(sum(section_strains))
-#print(max(sum(section_strains)**0.5)-0.1),0)
-#input()
+#print(max(section_strains))
+#print(sum(section_strains))
+print(((sum(section_strains)/7.5)**0.54))
+input()
