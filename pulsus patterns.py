bbf06ee8674e749bde88e2eb6d32f4fc71b4cdb3
Alexander Walker <alexanderwalker00@hotmail.com>
2023-03-21 23:13:00 +0000

    better pattern recognition + single pass sections

--- pulsus patterns.py
+++ pulsus patterns.py
@@ -9,88 +9,115 @@ for i in range(len(notes)): # get note data - position, time, hold, hold time (i
     temp = notes[i][1:-1].split(",")
     notes[i] = [int(temp[0]),int(float(temp[1])*(1000/rate)),(temp[2] == "1"),int(float(temp[3])*(1000/rate))]
 
-strain_notes = []
-hand = True
+
+first_hand = (notes[0][0] != 0)
+hand = bool(first_hand)
+hands = []
+streak = 0
+streaks = []
+streak_total = [0]
+
+for note in notes: # handing pass
+    if (note[0]%3 == 0 and hand) or (note[0]%3 == 2 and not hand):
+        hand = not hand
+        streak_total += [streak+streak_total[-1]]
+        streaks.append(streak)
+        streak = 0
+    hands.append(hand)
+    streak += 1
+if streak >= 1:
+    hand = not hand
+    streak_total += [streak+streak_total[-1]]
+    streaks.append(streak)
+
+for i in range(len(streaks)):
+    if streaks[i] > 4: # convert to full alt
+        for j in range(streak_total[i]+1,streak_total[i]+streaks[i],2):
+            hands[j] = not hands[j]
+streak = 0
+streaks = []
+streak_total = [0]
+
+for i in range(len(hands)): # get the new streaks
+    if (hands[i] != hands[i-1]) and i > 0:
+        streak_total += [streak+streak_total[-1]]
+        streaks.append(streak)
+        streak = 0
+    streak += 1
+if streak >= 1:
+    streak_total += [streak+streak_total[-1]]
+    streaks.append(streak)
+
+
+streak_multipliers = [0,1,0.9,1.1,1.2,1]
+note_multipliers = []
+hand = bool(first_hand)
+meta_streaks = [[0 for i in range(5)],[0 for i in range(5)]]
+hands = []
+
+for streak in streaks: # pattern multipliers 
+    multiplier = 0
+    current_streak = 0
+    for i in range(5):
+        if streak == i + 1:
+            current_streak = meta_streaks[hand][i] 
+            meta_streaks[hand][i] += 1
+        else:
+            meta_streaks[hand][i] = max(0,meta_streaks[hand][i]-(2+(meta_streaks[hand][i]//3)))
+    hands += [hand] * streak
+    hand = not hand
+    multiplier = (((0.9**(current_streak))+5)/6)**1 # crank the exponent up for lulz
+    note_multipliers += [multiplier*streak_multipliers[streak]]*streak
+
+#print(sum(note_multipliers)/len(note_multipliers))
 hold_stack = []
 strain_exp = [1,1]
 current_strain = [0,0]
 last_notes = [0,0]
 note_strain = 0
-map_length = 0
-max_holds = 0
-streak = 0
-streak_multipliers = [0,0.9,0.8,1.05,1.2,0.9]
-meta_streaks = [0,0]
-last_streaks = [0,0]
-
-for i in range(len(notes)): # handing / strain / pattern pass
-    note = notes[i]
-
-    # check and remove holds that have been released
-    hold_stack = [x for x in hold_stack if x > note[1]]
-
-    if (note[0]%3 == 0 and hand) or (note[0]%3 == 2 and not hand):
-        
-        if streak == last_streaks[hand]:
-            meta_streaks[hand] += 1
-            strain_notes, this_streak = strain_notes[:(streak * -1)], strain_notes[(streak * -1):]
-            this_streak = [x * (0.92 ** meta_streaks[hand]) for x in this_streak]
-            strain_notes += this_streak
-            current_strain[hand] *= 0.92 ** meta_streaks[hand]
-        else:
-            meta_streaks[hand] = 0
-        last_streaks[hand] = streak
-        
-        hand = not hand
-        streak = 0
-
-    streak += 1
-    if streak > 4 and streak%2 == 1: # flip hands for 5th, 7th, etc
-        hand = not hand
-
-    if i != 0:
-        if notes[i-1][1] + 20 > note[1]: # chord detection, with a small chording window
-            current_strain[hand] = (0.15 + current_strain[hand] / 1.2)
-        else: # do strain right
-            vert_notes = [note[0]//3,notes[last_notes[hand]][0]//3]
-            strain_exp[hand] = (note[1] - notes[last_notes[hand]][1]) / 1000
-            current_strain[hand] += (4 + (len(hold_stack) * 2)) * (streak_multipliers[min(streak,4)]) / ((15+strain_notes[-1]) - abs(vert_notes[0] - vert_notes[1]))
-            current_strain[hand] *= (0.66 ** strain_exp[hand])
-            last_notes[hand] = i
-    note_strain = current_strain[hand]
-
-    if streak > 4 and streak%2 == 1:
-        hand = not hand
- 
-    if note[2] == True: # hold
-        hold_stack.append(note[1] + note[3])
-        map_length = max(hold_stack[-1], map_length)
-    else:
-        map_length = max(note[1], map_length)
-
-    strain_notes.append(note_strain)
-
-note_times = [note[1] for note in notes]
-start = min(note_times)
+section_start = 0
 section_strains = []
 section = []
 
-while start <= map_length: # section pass
-    section = [x for x in note_times if (start <= x < start + 400)]
-    current_strain = 0
-    for note in section:
-        note_id = note_times.index(note)
-        current_strain += strain_notes[note_id]
-    section_strains.append(current_strain)
-    start += 400
+for i in range(len(notes)): # strain pass
+    note = notes[i]
+    hand = hands[i]
+    
+    # check and remove holds that have been released
+    hold_stack = [x for x in hold_stack if x > note[1]]
+    
+    if note[1] >= section_start + 400:
+        section_strains.append(sum(section))
+        section = []
+        section_start = note[1]
+    
+    if i != 0:
+        if notes[i-1][1] + 20 > note[1]: # chord detection, with a small chording window
+            note_strain = (0.15 + current_strain[hand] / 1.2)
+        else: # do strain right
+            note_strain = current_strain[hand]
+            vert_notes = [note[0]//3,notes[last_notes[hand]][0]//3]
+            strain_exp[hand] = (note[1] - notes[last_notes[hand]][1]) / 1000
+            current_strain[hand] += note_multipliers[i] * (4 + (len(hold_stack) * 2)) \
+                                    / ((15 + current_strain[hand]) - abs(vert_notes[0] - vert_notes[1]))
+            current_strain[hand] *= (0.66 ** strain_exp[hand])
+            last_notes[hand] = i
+    else:
+        note_strain = current_strain[hand] 
+ 
+    if note[2] == True: # hold
+        hold_stack.append(note[1] + note[3])
+
+    section.append(note_strain)
+
+section_strains.append(sum(section))
 
 section_strains.sort(reverse=True)
 section_strains = [x for x in section_strains if x > 0]
-for i in range(len(section_strains)):
-    section_strains[i] *= (0.92**i) # old method - may end up replacing with some form of line below
-    #section_strains[i] /= (4+i)
 
-#print(max(section_strains))
+for i in range(len(section_strains)):
+    section_strains[i] /= (4+i)
+
 #print(sum(section_strains))
-print(((sum(section_strains)/5.8)**0.52))
+print(((sum(section_strains)/1.7)**0.45))
 input()
